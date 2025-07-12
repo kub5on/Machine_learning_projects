de548778bb0986af48247c443c6ab02eb773cb2a
kub5on <j.paankowski@gmail.com>
2025-07-12 15:02:47 +0200

    payment anomalies project complited

--- Deep_learning 1/Payment_anomalies/main.py
+++ Deep_learning 1/Payment_anomalies/main.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import keras
+from tensorflow.keras.utils import set_random_seed
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import roc_auc_score, classification_report, roc_curve, confusion_matrix
 
-keras.utils.set_random_seed(43)
+set_random_seed(43)
 tf.config.experimental.enable_op_determinism()
 
 """wstępne przygotowanie danych"""
@@ -19,3 +25,45 @@ X_scaled = scaler.fit_transform(X)
 """podział na dane testowe i treningowe"""
 X_train = X_scaled[y == 0]
 X_test = X_scaled
+
+"""budowa enkodera"""
+input_dim = X_train.shape[1]
+encoding_dim = 14
+
+input_layer = Input(shape=(input_dim,))
+encoded = Dense(32, activation='relu')(input_layer)
+encoded = BatchNormalization()(encoded)
+encoded = Dropout(0.2)(encoded)
+
+encoded = Dense(encoding_dim, activation='relu')(encoded)
+
+"""budowa dekodera"""
+decoded = Dense(32, activation='relu')(encoded)
+decoded = BatchNormalization()(decoded)
+decoded = Dropout(0.2)(decoded)
+
+decoded = Dense(input_dim, activation='linear')(decoded)
+
+"autoenkoder"
+autoencoder = Model(input_layer, decoded)
+
+autoencoder.compile(optimizer=Adam(learning_rate=1e-3), loss='mse')
+
+"""trening modelu"""
+autoencoder.fit(X_train, X_train,
+                epochs=20,
+                batch_size=256,
+                shuffle=True,
+                validation_split=0.1)
+
+"""predykcje i blad rekonstrukcji"""
+X_test_pred = autoencoder.predict(X_test)
+mse = np.mean(np.power(X_test - X_test_pred, 2), axis=1)
+
+"""wykrywanie anomalii"""
+threshold = np.percentile(mse[y == 0], 95)
+y_pred = (mse > threshold).astype(int)
+
+roc_auc = roc_auc_score(y, mse)
+cr = classification_report(y, y_pred)
+# cm = confusion_matrix(y, y_pred)
--- Deep_learning 1/Payment_anomalies/plots.py
+++ Deep_learning 1/Payment_anomalies/plots.py
@@ -1,2 +1,20 @@
+from main import y, mse
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
+"""ROC curve"""
+fpr, tpr, thresholds = roc_curve(y, mse)
+roc_auc = auc(fpr, tpr)
+
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.legend(loc='lower right')
+plt.savefig('ROC_curve')
+plt.show()
 
