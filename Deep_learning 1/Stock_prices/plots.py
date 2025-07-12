4f63b9b668a27ac01242ac44f0076f1d4982f411
kub5on <j.paankowski@gmail.com>
2025-07-12 15:33:34 +0200

    names changed (main <--> plots)

--- Deep_learning 1/Stock_prices/plots.py
+++ Deep_learning 1/Stock_prices/plots.py
@@ -1,16 +1,1 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 
-df = pd.read_csv('sp500.txt')
-df["Date"] = pd.to_datetime(df["Date"])
-df = df.sort_values("Date")
-
-data = df["Close"].values
-
-train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False)
-
-scaler = MinMaxScaler()
-train_scaled = scaler.fit_transform(train_data.reshape(-1, 1))
-test_scaled = scaler.transform(test_data.reshape(-1, 1))
