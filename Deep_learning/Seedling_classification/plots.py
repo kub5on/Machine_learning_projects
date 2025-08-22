1599cc03130098656a5a79e9cf0ecfe1cc3cb468
kub5on <jaakub.pankowski@gmail.com>
2025-08-22 15:04:15 +0200

    ...

--- Deep_learning/Seedling_classification/plots.py
+++ Deep_learning/Seedling_classification/plots.py
@@ -4,21 +4,21 @@ import numpy as np
 import tensorflow as tf
 import keras
 
-# 1. Stała wartość seed
+# Stała wartość seed
 seed = 42
 
-# 2. Ustawienie seedów dla reprodukowalności
+# Ustawienie seedów dla reprodukowalności
 os.environ["PYTHONHASHseed"] = str(seed)
 random.seed(seed)
 np.random.seed(seed)
 tf.random.set_seed(seed)
 keras.utils.set_random_seed(seed)
 
-# 3. Wymuszenie deterministycznych operacji w TensorFlow
+# Wymuszenie deterministycznych operacji w TensorFlow
 os.environ["TF_DETERMINISTIC_OPS"] = "1"
 tf.config.experimental.enable_op_determinism()
 
-# 4. Parametryzacja
+# Parametryzacja
 train_dir = "plants_train"
 val_dir = "plants_test"
 
